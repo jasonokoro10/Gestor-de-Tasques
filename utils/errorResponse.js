3e34b422eb8575bc5e5fac42a755b1cd4bdc7cd8
jasonokoro10 <jasonozichukwu.okoro22@lacetania.cat>
2026-01-10 00:04:16 +0100

    docs: completada documentació i comentaris al codi

--- utils/errorResponse.js
+++ utils/errorResponse.js
@@ -1,8 +1,8 @@
-class ErrorResponse extends Error {
-    constructor(message, statusCode) {
-        super(message);
-        this.statusCode = statusCode;
+class ErrorResponse extends Error { // Estenem la classe Error nativa
+    constructor(message, statusCode) { // Constructor amb missatge i codi
+        super(message); // Passem missatge al pare
+        this.statusCode = statusCode; // Assignem codi d'estat HTTP
     }
 }
 
-module.exports = ErrorResponse;
+module.exports = ErrorResponse; // Exportem la classe
--- utils/generateToken.js
+++ utils/generateToken.js
@@ -1,9 +1,9 @@
-const jwt = require('jsonwebtoken');
+const jwt = require('jsonwebtoken'); // Importem llibreria JWT
 
-const generateToken = (id) => {
-    return jwt.sign({ id }, process.env.JWT_SECRET, {
-        expiresIn: process.env.JWT_EXPIRES_IN
+const generateToken = (id) => { // Funció per generar token
+    return jwt.sign({ id }, process.env.JWT_SECRET, { // Creem token signat amb ID
+        expiresIn: process.env.JWT_EXPIRES_IN // Configurem caducitat des de .env
     });
 };
 
-module.exports = generateToken;
+module.exports = generateToken; // Exportem la funció
